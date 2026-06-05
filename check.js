c16840d8fb846e29f136ea311b0f30cdc7799e4c
nwh <3055313094@qq.com>
2026-06-05 07:32:55 +0000

    2.7.4

--- check.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-try {
-  console.log(fs.readFileSync('lib/rules/utils/code-path-utils.js', 'utf8'));
-} catch (e) {
-  console.error(e);
-}
--- get_status.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-try {
-  const status = execSync('git status', { encoding: 'utf8' });
-  fs.writeFileSync('git_status.txt', status);
-} catch (e) {
-  fs.writeFileSync('git_status.txt', e.message);
-}
